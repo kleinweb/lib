5633a9bcbf76cb1a9acf69bdbad02e86f21a577a
Chris Montgomery <chrismont@temple.edu>
2025-04-11 17:36:17 -0400

    chore: reuse

--- src/Console/Commands/Attachment/DeleteDead.php
+++ src/Console/Commands/Attachment/DeleteDead.php
@@ -1,5 +1,8 @@
 <?php
 
+// SPDX-FileCopyrightText: (C) 2025 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 declare(strict_types=1);
 
 namespace Kleinweb\Lib\Console\Commands\Attachment;
