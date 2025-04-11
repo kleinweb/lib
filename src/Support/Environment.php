91706fcbb601883d49b09ede10288b8d847a3212
Chris Montgomery <chrismont@temple.edu>
2025-04-11 16:28:46 -0400

    feat: add support for 'migration' environment type

--- src/Support/Environment.php
+++ src/Support/Environment.php
@@ -11,6 +11,7 @@ namespace Kleinweb\Lib\Support;
 final class Environment
 {
     public const PRODUCTION = 'production';
+    public const MIGRATION = 'migration';
     public const STAGING = 'staging';
     public const DEVELOPMENT = 'development';
     public const LOCAL = 'local';
@@ -18,5 +19,12 @@ final class Environment
     public static function isProduction(): bool
     {
         return wp_get_environment_type() === self::PRODUCTION;
+    }
+
+    public static function isMigration(): bool
+    {
+        return defined('WP_ENV')
+            ? constant('WP_ENV') === self::MIGRATION
+            : false;
     }
 }
