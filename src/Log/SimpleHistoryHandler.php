c3431c2c7d2b47d7b15840bf9d65ab87e4121f2d
Chris Montgomery <chrismont@temple.edu>
2025-04-16 18:31:32 -0400

    feat(log): add simple-history plugin monolog handler

---
+++ src/Log/SimpleHistoryHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+// SPDX-FileCopyrightText: (C) 2025 Temple University <kleinweb@temple.edu>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+declare(strict_types=1);
+
+namespace Kleinweb\Lib\Log;
+
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\LogRecord;
+
+final class SimpleHistoryHandler extends AbstractProcessingHandler
+{
+    protected function write(LogRecord $record): void
+    {
+        $level = strtolower($record->level->getName());
+
+        \SimpleLogger()->$level($record->message, $record->context);
+    }
+}
