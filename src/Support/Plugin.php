4f715b6e61303695e15da21f01cf134910c3165e
Chris Montgomery <chrismont@temple.edu>
2025-10-10 12:07:32 -0400

    feat: add Plugin class

---
+++ src/Support/Plugin.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kleinweb\Lib\Support;
+
+use function ltrim;
+use function plugin_basename;
+use function plugin_dir_path;
+use function plugin_dir_url;
+
+final readonly class Plugin
+{
+    public string $basename;
+    public string $name;
+    public string $path;
+    public string $url;
+
+    final protected function __construct(
+        public string $file,
+        public string $version,
+    ) {
+        $this->basename = $this->basename();
+        $this->name = $this->basename;
+        $this->path = $this->path();
+        $this->url = $this->url();
+    }
+
+    /**
+     * Basename for the plugin.
+     */
+    final public function basename(): string
+    {
+        return plugin_basename($this->file);
+    }
+
+    /**
+     * URL for a file or directory within this plugin.
+     */
+    final public function url(string $file = ''): string
+    {
+        return plugin_dir_url($this->file) . ltrim($file, '/');
+    }
+
+    /**
+     * Filesystem path for a file or directory within this plugin.
+     */
+    final public function path(string $file = ''): string
+    {
+        return plugin_dir_path($this->file) . ltrim($file, '/');
+    }
+}
